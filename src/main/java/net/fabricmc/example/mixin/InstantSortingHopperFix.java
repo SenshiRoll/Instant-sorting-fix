b9ac879736391c38011ecbc876b174f8779d1000
SenshiRoll <littlebigman3488@gmail.com>
2020-07-07 21:15:19 -0400

    doesn't actually work but am tired

--- src/main/java/net/fabricmc/example/mixin/InstantSortingHopperFix.java
+++ src/main/java/net/fabricmc/example/mixin/InstantSortingHopperFix.java
@@ -1,40 +1,40 @@
 package net.fabricmc.example.mixin;
 
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.Hopper;
 import net.minecraft.block.entity.HopperBlockEntity;
 import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.util.math.Direction;
+
+import java.util.function.Supplier;
 
 import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HopperBlockEntity.class)
 public abstract class InstantSortingHopperFix extends LootableContainerBlockEntity {
+	@Shadow 
+	private int transferCooldown;
+
 	protected InstantSortingHopperFix(BlockEntityType<?> blockEntityType) {
 		super(blockEntityType);
 	}
 
-	@Inject(at = @At("HEAD"), method = "insert", cancellable = true)
-	private void Updates(CallbackInfoReturnable<Boolean> info) {
-		if (!this.world.isClient){
-			System.out.println("<"+this.world.getTime()+">  ["+this.pos.getX()+","+this.pos.getY()+","+this.pos.getZ()+"]");
-		}//
-	}
-	//@Overwrite       //so I can see a hopper tick
-	/*public void tick() {
-		if (this.world != null && !this.world.isClient) {
-		   --this.transferCooldown;
-		   this.lastTickTime = this.world.getTime();
-		   if (!this.needsCooldown()) {
-			  this.setCooldown(0);
-			  this.insertAndExtract(() -> {
-				 return extract(this);
-			  });
-		   }
-  
+	@Inject(at = @At("RETURN"), method = "insert", cancellable = true)
+	private void insertLog(CallbackInfoReturnable<Boolean> info) {
+		if (!this.world.isClient && this.transferCooldown==8){
+			System.out.println("I:  <"+this.world.getTime()+">  ["+this.pos.getX()+","+this.pos.getY()+","+this.pos.getZ()+"]");
 		}
-	}*/
-
+	}
+	@Inject(at=@At(value="INVOKE", target="extract"), method="tick", cancellable=true)
+	private void extractLog(CallbackInfo info){
+		if (!this.world.isClient && transferCooldown==8){
+			System.out.println("E:  <"+this.world.getTime()+">  ["+pos.getX()+","+this.pos.getY()+","+this.pos.getZ()+"]");
+		}
+	}
 }
