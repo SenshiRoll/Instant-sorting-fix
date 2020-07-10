873feb2754d3204b0234ab7a9a1a56f13963f976
SenshiRoll <littlebigman3488@gmail.com>
2020-07-10 16:16:47 -0400

    because update order

---
+++ src/main/java/net/fabricmc/example/mixin/HopperMapLog.java
@@ -0,0 +1,23 @@
+package net.fabricmc.example.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.block.entity.LootableContainerBlockEntity;
+
+@Mixin(HopperBlockEntity.class)
+public abstract class HopperMapLog extends LootableContainerBlockEntity {
+    protected HopperMapLog(BlockEntityType<?> blockEntityType) {
+        super(blockEntityType);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    private void log(CallbackInfo info){
+        System.out.println("I:  <"+this.world.getTime()+">  ["+this.pos.getX()+","+this.pos.getY()+","+this.pos.getZ()+"]");
+    }
+
+}
