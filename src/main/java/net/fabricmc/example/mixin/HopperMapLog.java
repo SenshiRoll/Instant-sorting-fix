35e1f2091e80693b73f870ec8a535eff32977aa4
SenshiRoll <littlebigman3488@gmail.com>
2020-08-02 15:02:40 -0400

    changed for HashSet bin tracking

--- src/main/java/net/fabricmc/example/mixin/HopperMapLog.java
+++ src/main/java/net/fabricmc/example/mixin/HopperMapLog.java
@@ -1,25 +1,97 @@
 package net.fabricmc.example.mixin;
 
+import java.util.function.BiFunction;
+import java.io.Serializable;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.block.entity.HopperBlockEntity;
-import net.minecraft.block.entity.LootableContainerBlockEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.ChunkManager;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.level.LevelProperties;
 
-@Mixin(HopperBlockEntity.class)
-public abstract class HopperMapLog extends LootableContainerBlockEntity {
-    //private static int wait=0;
-    protected HopperMapLog(BlockEntityType<?> blockEntityType) {
-        super(blockEntityType);
+@Mixin(ServerWorld.class)
+public abstract class HopperMapLog extends World {
+    protected HopperMapLog(LevelProperties levelProperties, DimensionType dimensionType, BiFunction<World, Dimension, ChunkManager> chunkManagerProvider, Profiler profiler, boolean isClient) {
+        super(levelProperties, dimensionType, chunkManagerProvider, profiler, isClient);
     }
+    @Inject(method="tick", at= @At("HEAD"), cancellable=false)
+    private void mapper(CallbackInfo info){
 
-    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
-    private void log(CallbackInfo info){
-        System.out.println("<"+this.world.getTime()+">  ["+this.pos.getX()+","+this.pos.getY()+","+this.pos.getZ()+"]");
-        //wait++;
     }
+    @Mixin(HashMap.class)
+    public abstract class HashMapMixin<K,V> extends AbstractMap<K,V> implements Serializable, Cloneable {
+        @Shadow
+        private static final long serialVersionUID = 362498820763181265L;
+    
+        @Shadow
+        transient Node<K,V>[] table;
+
+         /**
+         * Copypasta'd straight from HashMap
+         * 
+         * 
+         */
+        static class Node<K,V> implements Map.Entry<K,V> {
+            final int hash;
+            final K key;
+            V value;
+            Node<K,V> next;
+
+            Node(int hash, K key, V value, Node<K,V> next) {
+                this.hash = hash;
+                this.key = key;
+                this.value = value;
+                this.next = next;
+            }
+
+            public final K getKey()        { return key; }
+            public final V getValue()      { return value; }
+            public final String toString() { return key + "=" + value; }
+
+            public final int hashCode() {
+                return Objects.hashCode(key) ^ Objects.hashCode(value);
+            }
+            
+            public final V setValue(V newValue) {
+                V oldValue = value;
+                value = newValue;
+                return oldValue;
+            }
+
+            public final boolean equals(Object o) {
+                if (o == this)
+                    return true;
+                if (o instanceof Map.Entry) {
+                    Map.Entry<?,?> e = (Map.Entry<?,?>)o;
+                    if (Objects.equals(key, e.getKey()) &&
+                        Objects.equals(value, e.getValue()))
+                        return true;
+                }
+                return false;
+            }
+        }
+
+    @Inject(method="putVal", at= @At(value="INVOKE", target="newNode", ordinal=0))
+    public int getBin(int hash, K key, V value, boolean onlyIfAbsent, boolean evict, CallbackInfoReturnable<Integer> info){
+        int i;
+        i=(15 & hash);
+        return i;
+    }
+    
+}
 
 }
